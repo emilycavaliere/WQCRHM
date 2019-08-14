8725b28fa841bfa4c4a80d1fe6c01467cf022548
Kevin Shook <kevin.shook@usask.ca>
2019-08-14 09:34:16 -0600

    first commit

---
+++ R/WQCRHM-package.R
@@ -0,0 +1,10 @@
+#' @title Functions for CRHM water quality analyses
+#' @docType package
+#' @name WQCRHM
+#'
+#' @description \pkg{WQCRHM} is intended for water quality processing of CRHM output.
+
+#' @references
+#' To cite \pkg{WQCRHM} in publications, use the command \code{citation("WQCRHM")} to get the current version of the citation.\cr
+#'
+NULL
---
+++ R/getHRUparameters.R
@@ -0,0 +1,17 @@
+#' Reads parameters from CRHM .prj file
+#'
+#' @param prj_file Name of prj file
+#'
+#' @return Returns a list containing: HRU_name, HRU_SD_max, HRU_area
+#' @import CRHMr
+#' @export
+#'
+#' @examples \dontrun{HRU_params <- getHRUparameters("model.prj")}
+getHRUparameters <- function(prj_file) {
+  HRU_name <- readPrjHRUnames(prj_file)
+  HRU_SD_max <- readPrjParameters(prj_file, "Sdmax")
+  HRU_area <- readPrjParameters(prj_file, "hru_area")
+
+  returned <- list(HRU_name = HRU_name, HRU_SD_max = HRU_SD_max, HRU_area = HRU_area)
+  return(returned)
+}
