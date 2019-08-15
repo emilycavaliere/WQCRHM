a76cd8f83fef6cae7fb210516fa4496e48a31b52
Cavaliere <emc502@usask.ca>
2019-08-15 12:10:45 -0600

    Fixed the spacing

--- R/getHRUparameters.R
+++ R/getHRUparameters.R
@@ -11,7 +11,7 @@ getHRUparameters <- function(prj_file) {
   HRU_name <- readPrjHRUnames(prj_file)
   HRU_SD_max <- readPrjParameters(prj_file, "Sdmax")
   HRU_area <- readPrjParameters(prj_file, "hru_area")
-  HRU_volume<-HRU_SD_max*HRU_area
+  HRU_volume <-HRU_SD_max*HRU_area
 
   returned <- list(HRU_name = HRU_name, HRU_SD_max = HRU_SD_max, HRU_area = HRU_area)
   returned <- data.frame(HRU_name, HRU_SD_max, HRU_area, stringsAsFactors = FALSE)
