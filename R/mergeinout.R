285dbc1374acc3262016aacaa0e69acf596a51c9
Cavaliere <emc502@usask.ca>
2019-08-16 10:10:56 -0600

    Had to remove dplyr - created weird objects in output

--- R/mergeinout.R
+++ R/mergeinout.R
@@ -31,25 +31,23 @@ df$month<-month(df$date)
 
 #fix units:----
 
-df<-df%>%
-  group_by(date, hru) %>%
-  mutate(
-    condense=(condense/1000)*pond_area,
-    isd=(isd/1000)*pond_area,
-    isr=(isr/1000)*pond_area,
-    ri=(ri/1000)*pond_area,
-    mr=(mr/1000)*pond_area,
-    rre=(rre/1000)*pond_area,
-    rr=(rr/1000)*pond_area,
-    mi=(mi/1000)*pond_area,
-    gw=(gw/1000)*pond_area,
-    et=(et/1000)*pond_area,
-    sd=(sd/1000)*pond_area,
-    sgw=(sgw/1000)*pond_area,
-    sm=(sm/1000)*pond_area,
-    scr=(scr/1000)*pond_area,
-    sr=(sr/1000)*pond_area,
-    ssr=(ssr/1000)*pond_area)
+df$condense=(df$condense/1000)*df$pond_area
+df$isd=(df$isd/1000)*df$pond_area
+df$isr=(df$isr/1000)*df$pond_area
+df$ri=(df$ri/1000)*df$pond_area
+df$mr=(df$mr/1000)*df$pond_area
+df$rre=(df$rre/1000)*df$pond_area
+df$rr=(df$rr/1000)*df$pond_area
+df$mi=(df$mi/1000)*df$pond_area
+df$gw=(df$gw/1000)*df$pond_area
+df$et=(df$et/1000)*df$pond_area
+df$sd=(df$sd/1000)*df$pond_area
+df$sgw=(df$sgw/1000)*df$pond_area
+df$sm=(df$sm/1000)*df$pond_area
+df$scr=(df$scr/1000)*df$pond_area
+df$sr=(df$sr/1000)*df$pond_area
+df$ssr=(df$ssr/1000)*df$pond_area
+
 
 
 return(df)
