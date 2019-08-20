d77083611a361367d6b582caf0183f5fe4d2db9d
Cavaliere <emc502@usask.ca>
2019-08-20 08:20:14 -0600

    Added the out.rre into the mix - as outflows from the HRU.

--- R/wbcalc.R
+++ R/wbcalc.R
@@ -9,9 +9,10 @@
 wbcalc<-function(df){
 
   df$outflow = rowSums(cbind(df$sr,df$sgw,df$ssr,df$et))
-  head(df)
+  df$out.rre=as.numeric(df$sr-df$rre)#Main outflow variable from a wetland is soil runoff ( ), but a portion of this returns back as  redirected residual
   df$inflow = rowSums(cbind(df$mr, df$mi, df$rr, df$ri, df$condense,df$isd,df$isr, df$rre), na.rm = T)
-df$out.rre<-as.numeric(df$out-df$rre)#Main outflow variable from a wetland is soil runoff ( ), but a portion of this returns back as  redirected residual
 f.df<-df
 return(f.df)
 }
+
+
