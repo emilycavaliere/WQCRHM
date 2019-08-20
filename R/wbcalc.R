7e76c31aa6546d2f2c5f188e31fdb0199b829e90
Cavaliere <emc502@usask.ca>
2019-08-20 08:58:34 -0600

    Fixed waterbalance again

--- R/wbcalc.R
+++ R/wbcalc.R
@@ -8,9 +8,11 @@
 #' @examples \dontrun
 wbcalc<-function(df){
 
-  df$outflow = rowSums(cbind(df$sr,df$sgw,df$ssr,df$et))
+  #df$outflow = rowSums(cbind(df$sr,df$sgw,df$ssr,df$et))
+  df$outflow = as.numeric(df$sr+df$sgw+df$ssr+df$et)
   df$out.rre=as.numeric(df$sr-df$rre)#Main outflow variable from a wetland is soil runoff ( ), but a portion of this returns back as  redirected residual
-  df$inflow = rowSums(cbind(df$mr, df$mi, df$rr, df$ri, df$condense,df$isd,df$isr, df$rre), na.rm = T)
+  #df$inflow = rowSums(cbind(df$mr, df$mi, df$rr, df$ri, df$condense,df$isd,df$isr, df$rre), na.rm = T)
+  df$inflow = as.numeric(df$mr+ df$mi+df$rr+ df$ri+df$condense+df$isd+df$isr+df$rre)
 f.df<-df
 return(f.df)
 }
